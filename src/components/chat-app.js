12c9d1f83e24830c84e4a16443b33b634dd32a50
waxime <aveline.maxime@gmail.com>
2019-06-04 14:40:08 +0200

    Manage app loaded

---
+++ src/components/chat-app.js
@@ -0,0 +1,52 @@
+import { LitElement, html } from 'lit-element';
+import './chat-data.js';
+
+class ChatApp extends LitElement {
+
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  static get properties(){
+    return {
+      unresolved: {
+        type: Boolean,
+        reflect: true
+      },
+      email: { type: String },
+      pseudo: { type: String },
+      data: { type: Array}
+    }
+  }
+
+  firstUpdated() {
+    this.unresolved = false;
+  }
+
+  handleForm(e) {
+    e.preventDefault();
+    const email = this.shadowRoot.querySelector('input[name=email]');
+    const pseudo = this.shadowRoot.querySelector('input[name=pseudo]');
+
+    this.shadowRoot.querySelector('#data').push({
+      email: email.value,
+      pseudo: pseudo.value
+    })
+  }
+
+  customChildAdded(e) {
+    this.data = [
+      ...this.data,
+      e.detail
+    ];
+  }
+
+  render() {
+    return html`     
+    App loaded
+    `
+  }
+}
+
+customElements.define('chat-app', ChatApp);
